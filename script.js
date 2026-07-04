ca2a91c9bbdd69304f3e63e3a7365b03196a1fa2
wabsit <gsah53503@gmail.com>
2026-07-04 14:19:37 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+async function generate() {
+  const prompt = document.getElementById("prompt").value;
 
+  const res = await fetch("/api/generate", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ prompt })
+  });
+
+  const data = await res.json();
+  document.getElementById("output").innerText = data.result;
+}
