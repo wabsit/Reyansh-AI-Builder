214b373517a91de2089f8f2fbfc8771dc6584c91
wabsit <gsah53503@gmail.com>
2026-07-11 13:03:11 +0530

    editor.js
    Added HTML template loading and preview functionality.

--- editor.js
+++ editor.js
@@ -1,1 +1,41 @@
-console.log("Editor Loaded");
+// =====================================
+// Reyansh AI Builder Pro
+// Editor Part A
+// =====================================
+
+const editor = document.getElementById("codeEditor");
+const preview = document.getElementById("preview");
+
+function loadHTML() {
+
+    editor.value =
+`<!DOCTYPE html>
+<html>
+<head>
+<title>Reyansh AI Builder</title>
+</head>
+
+<body>
+
+<h1>Hello World</h1>
+
+</body>
+</html>`;
+
+    preview.srcdoc = editor.value;
+
+}
+
+function updatePreview(){
+
+    preview.srcdoc = editor.value;
+
+}
+
+if(editor){
+
+editor.addEventListener("keyup", updatePreview);
+
+loadHTML();
+
+}
